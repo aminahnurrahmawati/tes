310bfb33097b6f2b14f8acf8d8b4368eb1f48cdf
Aminah Nurrahmawati <104577829+aminahnurrahmawati@users.noreply.github.com>
2023-12-20 16:10:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+my_words = ["Hi!", "Python Pro", "Kodland"] 
+print(my_words[1])
 
+
+###############################################
+
+
+name = input()
+print("Hi ", name)
+
+
+
+################################################
+
+import random
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+print(random.choice(emojis))
